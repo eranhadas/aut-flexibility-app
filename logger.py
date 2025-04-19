a78fb86605d4a8248ee14bc654efe990335e8ff8
Eran Hadas <ehadas@gmail.com>
2025-04-19 15:20:34 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -11,6 +11,8 @@ USE_SHEETS = True
 def _init_sheet():
     try:
         credentials_dict = json.loads(st.secrets["google"]["credentials"])
+        # Fix the private key format
+        credentials_dict["private_key"] = credentials_dict["private_key"].replace("\\n", "\n")
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
         client = gspread.authorize(creds)
